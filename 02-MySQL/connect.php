de189554f83320c8f6061b3cc90041db01c8df04
Jan Koupil <jan.koupil@gmail.com>
2022-10-18 16:43:50 +0200

    2022-10-18 Connect MySQL

---
+++ 02-MySQL/connect.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap-->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Připojení k DB</title>
+</head>
+<body>
+<?php
+$host = '127.0.0.1';
+$db = 'ip_3';
+$user = 'www-aplikace';
+$pass = 'Bezpe4n0Heslo.';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query('SELECT room_id, name, phone FROM room');
+
+        if ($stmt->rowCount() == 0) {
+            echo "Záznam neobsahuje žádná data";
+        }
+        else
+        {
+            while ($row = $stmt->fetch()) { //nebo foreach ($stmt as $row)
+                var_dump($row);
+            }
+        }
+        unset($stmt);
+?>
+</body>
+</html>
